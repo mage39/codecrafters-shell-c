4464acb8f5868b97f2c0c38828c5e3883c847956
mage39 <106197576+mage39@users.noreply.github.com>
2024-08-19 15:51:38 -0500

    handle missing commands

--- app/main.c
+++ app/main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
+	// Uncomment this block to pass the first stage
+	printf("$ ");
+	fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  return 0;
+	// Wait for user input
+	char input[100];
+	fgets(input, 100, stdin);
+
+	for (int i = 0; i != ' ' && i != NULL && i < 100; i++) {
+		print("%c", input[i]);
+	}
+	print(": command not found\n");
+
+	return 0;
 }
