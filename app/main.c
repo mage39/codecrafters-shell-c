a42f76a25c45f53e01dc0f06ab84828d857c2463
mage39 <106197576+mage39@users.noreply.github.com>
2024-08-20 15:40:02 -0500

    stage 3 passed

--- app/main.c
+++ app/main.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <string.h>
-#include <regex.h>
 #include <stdio.h>
 
 int main() {
@@ -12,28 +11,21 @@ int main() {
 	char input[100];
 	fgets(input, 100, stdin);
 
-	regex_t regex;
-	regmatch_t offsets = {0};
-	int err = regcomp(&regex, "^.[[:alnum:]]+", REG_NEWLINE | REG_EXTENDED);
-	if (err) {
-		printf("excweeze me?\n");
-		exit(1);
+	int cmdLen = 0;
+	for (; cmdLen < 100; cmdLen++) {
+		switch (input[cmdLen]) {
+			case 'a'...'z':
+			case 'A'...'Z':
+			case '0'...'9':
+				break;
+			default:
+				goto cmdEnd;
+		}
 	}
-	err = regexec(&regex, input, 0, &offsets, 0);
-	char output[100] = {0};
-	if (!err) {
-		size_t len = offsets.rm_eo - offsets.rm_so; // end offset not calculated at true end of match
-		strncpy(output, input + offsets.rm_so, len);
-	}
-	else if (err == REG_NOMATCH) printf("no match, WTF\n");
-	else {
-		char buff[100];
-		regerror(err, &regex, buff, sizeof(buff));
-		printf("regex failed %s\n", buff);
-		exit(1);
-	}
-	printf("%s: command not found\n", output);
+cmdEnd:;
+	char cmd[100] = {0};
+	strncpy(cmd, input, cmdLen);
+	printf("%s: command not found\n", cmd);
 
-	regfree(&regex);
 	return 0;
 }
