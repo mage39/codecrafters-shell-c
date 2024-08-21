df7490222e16fab2e29c04a482d2d498def9c127
mage39 <106197576+mage39@users.noreply.github.com>
2024-08-20 19:24:00 -0500

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -12,16 +12,48 @@ int main() {
 		char input[100];
 		fgets(input, 100, stdin);
 
-		char cmd[100] = "exit";
+		char cmd[100] = "exit ";
 		int cmdLen = strlen(cmd);
 		if (!strncmp(input, cmd, cmdLen)) {
 			int ret = atoi(input + cmdLen);
 			return ret;
 		}
-		strncpy(cmd, "echo", 100);
+		strncpy(cmd, "echo ", 100);
 		cmdLen = strlen(cmd);
 		if (!strncmp(input, cmd, cmdLen)) {
-			printf("%s", input + cmdLen + 1);
+			printf("%s", input + cmdLen);
+			continue;
+		}
+		strncpy(cmd, "type ", 100);
+		cmdLen = strlen(cmd);
+		if (!strncmp(input, cmd, cmdLen)) {
+			int typeLen;
+			char builtins[][25] = {"exit", "echo", "type"};
+			for (int i = 0; i < 3; i++) {
+				typeLen = strlen(builtins[i]);
+				if (!strncmp(input + cmdLen, builtins[i], typeLen)) {
+					printf("%s is a shell builtin\n", builtins[i]);
+					goto whileContinue;
+				}
+			}
+			typeLen = 0;
+			for (; cmdLen < 100; cmdLen++) {
+				switch (input[cmdLen]) {
+					case 'a'...'z':
+					case 'A'...'Z':
+					case '0'...'9':
+					case '-'...'.':
+					case '_':
+						typeLen++;
+						break;
+					default:
+						goto typeEnd;
+				}
+			}
+typeEnd:
+			cmdLen = strlen(cmd);
+			strncpy(cmd, input + cmdLen, typeLen);
+			printf("%s: not found\n", cmd);
 			continue;
 		}
 
@@ -41,6 +73,7 @@ int main() {
 cmdEnd:;
 		strncpy(cmd, input, cmdLen);
 		printf("%s: command not found\n", cmd);
+whileContinue:;
 	}
 
 	return 0;
