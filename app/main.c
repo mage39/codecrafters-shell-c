9987acd7528ec9c3f4ed0c7306fedfec5946e701
mage39 <106197576+mage39@users.noreply.github.com>
2024-11-19 06:15:35 -0600

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -15,7 +15,7 @@ static const char* validChars = "abcdefghijklmnopqrstuvwxyz"
 
 static char* which (const char* cmd) {
 	const char* pathEnv = getenv("PATH");
-	char* path = pathEnv;
+	char* freePath = strdup(pathEnv), *path = freePath;
 	char* ret = malloc(LEN);
 	if (!ret) {
 		perror("malloc failed: in function which");
@@ -28,11 +28,13 @@ static char* which (const char* cmd) {
 			if (!strcmp(t->d_name, cmd)) {
 				closedir(pathDir);
 				snprintf(ret, LEN, "%s/%s", tok, cmd);
+				free(freePath);
 				return ret;
 			}
 		}
 		closedir(pathDir);
 	}
+	free(freePath);
 	free(ret);
 	return 0;
 }
